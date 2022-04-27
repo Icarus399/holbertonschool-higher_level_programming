96605e8663240952d106386b28b4dc23b4d5b17f
Icarus399 <3530@holbertonschool.com>
2022-04-27 03:39:25 +0000

    adding files from task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    up = ""
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+
+                up += chr(ord(i) - 32)
+        else:
+                up += i
+                print('{}'.format(up))
