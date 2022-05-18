77967e1a6de85d565bff19194c113a3c0729afd1
Icarus399 <3530@holbertonschool.com>
2022-05-18 01:05:38 +0000

    adding files

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" Module with the class Square"""
+""" Module with class Square"""
 
 
 class Square:
-    """ Square Defined """
+    """ square Defined """
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module contain class square """
+
+
+class Square:
+    """ Defined Square """
+    def __init__(self, size):
+        self.__size = size
